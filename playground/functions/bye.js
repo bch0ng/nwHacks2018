0c454fdcb3582d882991fd320cc4745a3a1d3298
bch0ng <bchong@uw.edu>
2018-01-14 04:21:58 -0800

    working

--- playground/functions/bye.js
+++ playground/functions/bye.js
@@ -1,10 +1,9 @@
 /**
 * Geoca
-* @param {float} lat
-* @param {float} long
+* @param {String} emergencyID
 * @returns {any}
 */
-module.exports = (lat, long, context, callback) => {
+module.exports = (emergencyID, context, callback) => {
 	var admin = require("firebase-admin");
 
 	// Initialize the app with a service account, granting admin privileges
@@ -24,14 +23,36 @@ module.exports = (lat, long, context, callback) => {
 	  for (var x in data) {
 	  	availableDispatcher.push(x);
 	  }
-	  closestDispatcher(lat, long, ref, data, db, availableDispatcher)
+	  var emerLat;
+	  var emerLong;
+	  var closestID;
+	  emergencyLocation(emergencyID, ref, db)
+	  .then(function(result) {
+			emerLat = result['latitude'];
+			emerLong = result['longitude'];
+	  }).then(function() {
+	  	closestID = closestDispatcher(emerLat, emerLong, ref, data, db, availableDispatcher);
+		}).then(function() {
+			statusUpdater(closestID, ref, db)
+	  .then(function(result) {
+	  	callback(closestID);
+	  	process.exit();
+	  });
+		})
+	  /*
 	  .then(function(result) {
 	  	var data = result;
 	  }).then(function() {
 	  	process.exit();
 	  });
-	});
 
+	  statusUpdater(closestID, ref, db)
+	  .then(function(result) {
+	  	callback(closestID);
+	  	process.exit();
+	  });
+	  */
+	});
 };
 
 function allDispatchers(ref) {
@@ -43,8 +64,48 @@ function allDispatchers(ref) {
   });
 };
 
-function closestDispatcher(lat, long, ref, data, db, availableDispatcher) {
-	return new Promise(function(resolve, reject) { 
+function emergencyLocation(emergencyID, ref, db) {
+	return new Promise(function(resolve, reject) {
+		// Requires the - before id because it cant be inputted into terminal :(
+		ref = db.ref('Emergency/-' + emergencyID + '/location');
+		allDispatchers(ref).then(function(result) {
+			resolve(result);
+		});
+	});
+}
+
+function closestDispatcher(emerLat, emerLong, ref, data, db, availableDispatcher) {
+  	// JS Max Int
+  	var closestDistance = 4294967295;
+  	var closestDispatcherInArea = '';
+  	for (var x in data) {
+  		if (data[x]['Status'] == 'a') {
+	  		var dispatchLat = data[x]['Location'].Latitude;
+	  		var dispatchLong = data[x]['Location'].Longitude;
+	  		var distanceBetween = distance(emerLat, emerLong, dispatchLat, dispatchLong);
+	  		if (distanceBetween < closestDistance) {
+	  			closestDispatcherInArea = x;
+	  			closestDistance = distanceBetween;
+	  		}
+	  	}
+  	}
+  	return closestDispatcherInArea;
+};
+
+function statusUpdater(dispatchID, ref, db) {
+	return new Promise(function(resolve, reject) {
+		ref = db.ref('Dispatchers').child(dispatchID);
+		ref.update({
+			Status: 'b'
+		}).then(function() {
+			resolve(true);
+		});
+	});
+}
+
+/*
+function closestDispatcher(emergencyID, ref, data, db, availableDispatcher) {
+	return new Promise(function(resolve, reject) {
 		var size = availableDispatcher.length;
   	var index = 0;
   	var found = false;
@@ -65,3 +126,35 @@ function closestDispatcher(lat, long, ref, data, db, availableDispatcher) {
 		//resolve(result);
 	});
 };
+*/
+
+/*
+function closestDispatcherFinder(lat, long) {
+var location = db.ref('Emergency/location');
+var emerLat = 
+var emerLong = 
+var closestDispatcherDistance;
+
+var dispatcher = availableDispatcher[0];
+closestDispatcher = dispatcher; 
+distance = distance(lat, lon, dispatcher[latitude], dispatcher[longitude])
+
+for (var i = 1; i < size; i++) {
+  var dispatcher2 = availableDispatcher[i];
+var acc = distance(emerLat, emerLong, lat, long)
+ if ( acc < distance) {
+   distance = acc;
+   closestDispatcher = dispatcher;
+ }
+}
+*/
+
+function distance(lat1, lon1, lat2, lon2) {
+	var p = 0.017453292519943295;    // Math.PI / 180
+	var c = Math.cos;
+	var a = 0.5 - c((lat2 - lat1) * p)/2 +
+	       c(lat1 * p) * c(lat2 * p) *
+	       (1 - c((lon2 - lon1) * p))/2;
+
+	return 12742 * Math.asin(Math.sqrt(a));
+}
