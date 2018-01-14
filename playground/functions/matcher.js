ca9c7fbee3dce78483bebc497aa59f5024e45e14
Shusanta Bhattarai <shusantabhattarai@Shusantas-MacBook-Pro.local>
2018-01-13 20:04:04 -0800

    need to parse json

--- playground/functions/matcher.js
+++ playground/functions/matcher.js
@@ -2,38 +2,22 @@
  * Matches an emergency caller with an available dispatcher.
  *
  */
+const https = require("https");
 
 module.exports = (context, callback) => {
 
-	// Read the Database
-
-	var admin = require("firebase-admin");
-	admin.database.enableLogging(true);
-	var serviceAccount = require("../serviceAccountKey.json");
-	admin.initializeApp({
-	  credential: admin.credential.cert(serviceAccount),
-	  databaseURL: "https://nwhacks-7c19a.firebaseio.com"
+	const https = require("https");
+	const url =
+	  "https://nwhacks-7c19a.firebaseio.com/Dispatchers.json";
+	https.get(url, res => {
+	  res.setEncoding("utf8");
+	  let body = "";
+	  res.on("data", data => {
+	    body += data;
+	  });
+	  res.on("end", () => {
+	    body = JSON.parse(body);
+	    console.log(body);
+	  });
 	});
-
-	var db = admin.database();
-	console.log('lol');
-	var ref = db.ref("/Dispatchers");
-	var data;
-	ref.on("value", function(snapshot) {
-	  data = callback(snapshot.val());
-	});
-
-	/*
-	if (userID == null) {	// Check if userID is present in database
-			throw new Error('Missing userID');
-	}
-	if (disID == null) { // Check if disID is present in database
-		throw new Error('Missing disID');
-	}
-  return callback(null, `bye`);
-  */
-
-  //callback(curl.getJSON('https://nwhacks-7c19a.firebaseio.com/Dispatchers.json');
-
-
 };
